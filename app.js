6c37f94b0a2b5a453f7fed1c10104f7ea2457455
Collins <collinsleewyatt@gmail.com>
2020-04-16 20:06:46 -0400

    Removed a stray console log I forgot I put in

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ import {MDCRipple} from '@material/ripple/index';
 
 //See https://github.com/material-components/material-components-web/tree/master/packages/mdc-ripple
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
-console.log("greetings");
