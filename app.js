66b0c3397c77ff44d46b7a7072ceb025af5d8b31
Collins <collinsleewyatt@gmail.com>
2020-04-16 20:03:23 -0400

    Updated packages, removed node-sass in favor of sass, changed webpack configuration for more parity with the getting started guide, and added a few helpful comments leading people to the official documentation.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 import {MDCRipple} from '@material/ripple/index';
 
+//See https://github.com/material-components/material-components-web/tree/master/packages/mdc-ripple
 const ripple = new MDCRipple(document.querySelector('.foo-button'));
+console.log("greetings");
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const autoprefixer = require('autoprefixer');
-
 module.exports = [{
   entry: ['./app.scss', './app.js'],
   output: {
-    filename: 'bundle.js',
+     filename: 'bundle.js'
   },
   module: {
     rules: [
@@ -13,20 +12,24 @@ module.exports = [{
           {
             loader: 'file-loader',
             options: {
-              name: 'bundle.css',
-            },
+              name: 'bundle.css'
+            }
           },
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
           {
             loader: 'postcss-loader',
             options: {
-               plugins: () => [autoprefixer()]
+              plugins: () => [autoprefixer()]
             }
           },
           {
             loader: 'sass-loader',
             options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
               includePaths: ['./node_modules']
             }
           }
@@ -37,7 +40,7 @@ module.exports = [{
         loader: 'babel-loader',
         query: {
           presets: ['@babel/preset-env']
-        },
+        }
       }
     ]
   },
